7013ba18c225ef98210c0e577680e0e78fb34e38
0richalcos <xox.zhe@foxmail.com>
2021-08-06 00:37:46 +0800

    2021/08/06/00:37

---
+++ springcloud_parent/springcloud_user/src/main/java/com/orichalcos/config/BeansConfig.java
@@ -0,0 +1,15 @@
+package com.orichalcos.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class BeansConfig {
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
--- springcloud_parent/springcloud_user/src/main/java/com/orichalcos/controller/UserController.java
+++ springcloud_parent/springcloud_user/src/main/java/com/orichalcos/controller/UserController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +23,13 @@ public class UserController {
     @Autowired
     private DiscoveryClient discoveryClient;
 
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+
     @GetMapping
     public String invokeDemo() {
         LOGGER.info("user demo...");
@@ -35,17 +43,36 @@ public class UserController {
     }
 
     @GetMapping("/discoveryClient")
-    public String discoveryClient(){
+    public String discoveryClient() {
+        //获取服务列表
         List<ServiceInstance> orders = discoveryClient.getInstances("order");
-        orders.forEach(order->{
-            LOGGER.info("服务主机：【{}】",order.getHost());
-            LOGGER.info("服务端口：【{}】",order.getPort());
+        orders.forEach(order -> {
+            LOGGER.info("服务主机：【{}】", order.getHost());
+            LOGGER.info("服务端口：【{}】", order.getPort());
             LOGGER.info("服务地址：【{}】", order.getUri());
         });
         //从服务列表中随机调取一个服务
         ServiceInstance order = orders.get(new Random().nextInt(orders.size()));
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(order.getUri() + "/order", String.class);
+        return "User服务调用OK，" + result;
+    }
+
+    @GetMapping("/loadBalancerClient")
+    public String loadBalancerClient() {
+        //根据负载均衡策略选取某一个服务调用
+        ServiceInstance order = loadBalancerClient.choose("order");
+        LOGGER.info("服务主机：【{}】", order.getHost());
+        LOGGER.info("服务端口：【{}】", order.getPort());
+        LOGGER.info("服务地址：【{}】", order.getUri());
+        RestTemplate restTemplate = new RestTemplate();
+        String result = restTemplate.getForObject(order.getUri() + "/order", String.class);
+        return "User服务调用OK，" + result;
+    }
+
+    @GetMapping("/loadBalanced")
+    public String loadBalanced() {
+        String result = restTemplate.getForObject("http://order/order", String.class);
         return "User服务调用OK，" + result;
     }
 }
